a52a9cda9a7cfd3d5cd180acc6ea05326c85f821
ryudai <bigdragon.06102001@gmail.com>
2022-04-27 03:38:50 +0900

    chatに認証をはさみました。

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/chat', 'ChatController@index')->name('chat');
+Route::get('/chat', 'ChatController@index')->name('chat')->middleware('auth');
