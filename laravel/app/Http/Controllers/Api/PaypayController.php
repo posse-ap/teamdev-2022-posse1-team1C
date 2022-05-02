74ff3bb0a91619c7bf5f452808b8f0e5c5be73cd
ryudai <bigdragon.06102001@gmail.com>
2022-05-03 02:51:00 +0900

    paypayテスト実装。スマホ決済のみ可能。

---
+++ laravel/app/Http/Controllers/Api/PaypayController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use PayPay\OpenPaymentAPI\Client;
+use PayPay\OpenPaymentAPI\Models\CreateQrCodePayload;
+use PayPay\OpenPaymentAPI\Models\OrderItem;
+
+class PaypayController extends Controller
+{
+  public function index()
+  {
+    $client = new Client([
+      'API_KEY' => env('PAYPAY_API_KEY'),
+      'API_SECRET' => env('PAYPAY_API_SECRET'),
+      'MERCHANT_ID' => env('PAYPAY_MERCHANT_ID'),
+    ], false);
+
+    /*
+           .....initialize SDK
+           */
+    // setup payment object
+    $CQCPayload = new CreateQrCodePayload();
+
+    // Set merchant pay identifier
+    $CQCPayload->setMerchantPaymentId("Mr_Kotani_please_help_me");
+
+    // Log time of request
+    $CQCPayload->setRequestedAt();
+    // Indicate you want QR Code
+    $CQCPayload->setCodeType("ORDER_QR");
+
+    // Provide order details for invoicing
+    $OrderItems = [];
+    $OrderItems[] = (new OrderItem())
+      ->setName('Cake')
+      ->setQuantity(1)
+      ->setUnitPrice(['amount' => 20, 'currency' => 'JPY']);
+    $CQCPayload->setOrderItems($OrderItems);
+
+    // Save Cart totals
+    $amount = [
+      "amount" => 1,
+      "currency" => "JPY"
+    ];
+    $CQCPayload->setAmount($amount);
+    // Configure redirects
+    $CQCPayload->setRedirectType('WEB_LINK');
+    $CQCPayload->setRedirectUrl('https://paypay.ne.jp/');
+
+    $CQCPayload->setIsAuthorization(false);
+    $CQCPayload->setUserAgent($_SERVER['HTTP_USER_AGENT']);
+
+    // Get data for QR code
+    $response = $client->code->createQRCode($CQCPayload);
+
+    $data = $response['data'];
+    return json_encode($data);
+  }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/chat', 'Api\ChatController@index');
 Route::post('/chat', 'Api\ChatController@store');
+
+Route::get('/paypay', 'Api\PaypayController@index');
