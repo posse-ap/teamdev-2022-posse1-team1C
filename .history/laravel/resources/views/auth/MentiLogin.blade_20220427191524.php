092132a41961d206e42c529837afe8900318af32
ryudai <bigdragon.06102001@gmail.com>
2022-05-04 20:19:08 +0900

    不要なファイル類を削除

--- .history/laravel/resources/views/auth/MentiLogin.blade_20220427191524.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.base')
- 
-@section('title', 'コンタクト画面')
- 
-@section('content')
-<p>コンタクト画面です</p>
-@endsection
--- .history/laravel/resources/views/auth/MentiRegister.blade_20220427191955.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.BeforeLoginBase')
-
-@section('title', 'メンティーの登録画面')
-
-@section('content')
-<p>ここに登録フォームを書く</p>
-@endsection
