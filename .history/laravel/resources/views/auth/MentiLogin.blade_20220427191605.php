4ff42c3e757eb8ab68e1024d5342000e4a6cc948
ryudai <bigdragon.06102001@gmail.com>
2022-05-04 20:32:33 +0900

    不要なファイルを削除

--- .history/laravel/resources/views/auth/MentiLogin.blade_20220427191605.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.BeforeLogin')
- 
-@section('title', 'メンティーのログイン画面')
- 
-@section('content')
-<p>コンタクト画面です</p>
-@endsection
--- .history/laravel/resources/views/includes/BeforeLoginHeader.blade_20220427201759.php
+++
@@ -1,6 +0,0 @@
-<form action="" method="get">
-    <input class="" type="text" name="name" value="" placeholder="社名を検索">
-    <input type="submit" value="検索">
-</form>
-<button class="bg-[#13B1C0] text-white p-2 rounded-md shadow-md h-10 my-auto ml-auto">ログイン</button>
-<button class="bg-[#13B1C0] text-white p-2 rounded-md shadow-md h-10 my-auto ml-20">無料登録</button>
