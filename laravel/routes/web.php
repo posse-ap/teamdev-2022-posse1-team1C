71a25211356de5aa5c86e024f0a7cbe66ae1c7ac
ryudai <bigdragon.06102001@gmail.com>
2022-05-06 03:18:36 +0900

    メンティー側のカレンダーの見た目を作成

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,6 +19,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/MenteeRegister', 'HomeController@MenteeRegister')->name('home');
+Route::get('/mentee/register', 'HomeController@MenteeRegister')->name('mentete.register');
 
 Route::get('/chat', 'ChatController@index')->name('chat')->middleware('auth');
+
+Route::get('/schedule-test', function () {
+  return view('schedule.index');
+});
