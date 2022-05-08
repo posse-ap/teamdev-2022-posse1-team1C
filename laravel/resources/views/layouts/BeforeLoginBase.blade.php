3938998ffe8ffee31289d54f21645d5c914d86f8
junjan-061 <junjan0601@icloud.com>
2022-05-08 16:57:15 +0900

    メンター検索画面の見た目を作成

--- laravel/resources/views/layouts/BeforeLoginBase.blade.php
+++ laravel/resources/views/layouts/BeforeLoginBase.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
 </head>
 
 <body>
