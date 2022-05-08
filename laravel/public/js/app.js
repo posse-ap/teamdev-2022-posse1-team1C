a77c32ec829fb0dbbaca8e3879fba20e333184b6
chiyoko-hayashi <chiyoko.hayashi@harbors.sh>
2022-05-08 17:55:48 +0900

    全てindex.blade.phpに統合

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -37382,9 +37382,12 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/**
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
   \*********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\n/work/backend/tailwind.config.js:10\n            -       '-180': '-180deg',\n            ^\n\nSyntaxError: Unexpected token '-'\n    at Object.compileFunction (node:vm:352:18)\n    at wrapSafe (node:internal/modules/cjs/loader:1032:15)\n    at Module._compile (node:internal/modules/cjs/loader:1067:27)\n    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)\n    at Module.load (node:internal/modules/cjs/loader:981:32)\n    at Function.Module._load (node:internal/modules/cjs/loader:822:12)\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\n    at require (node:internal/modules/cjs/helpers:102:18)\n    at getTailwindConfig (/work/backend/node_modules/tailwindcss/lib/lib/setupTrackingContext.js:116:53)\n    at /work/backend/node_modules/tailwindcss/lib/lib/setupTrackingContext.js:21:92\n    at processResult (/work/backend/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /work/backend/node_modules/webpack/lib/NormalModule.js:860:5\n    at /work/backend/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /work/backend/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/work/backend/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/work/backend/node_modules/postcss-loader/dist/index.js:142:7)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -70460,7 +70463,42 @@ if (false) {} else {
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -70522,13 +70560,68 @@ if (false) {} else {
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/app": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	__webpack_require__("./resources/js/app.js");
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = __webpack_require__("./resources/sass/app.scss");
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
 ;
--- laravel/resources/views/call/index.blade.php
+++ laravel/resources/views/call/index.blade.php
@@ -1,84 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.webrtc.ecl.ntt.com/skyway-4.4.4.js"></script>
-    <title>SkyWayチュートリアル</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
+    <title>呼び出し中</title>
 </head>
 
-
 <body>
-    <video id="my-video" width="400px" autoplay muted playsinline></video>
-    <p id="my-id"></p>
-    <!-- 相手のPeerIDを記入するテキストエリア -->
-    <input id="their-id"></input>
-    <!-- 発信ボタン -->
-    <button id="make-call">発信</button>
-    <!-- 着信時に相手の映像を表示するvideo要素 -->
-    <video id="their-video" width="400px" autoplay muted playsinline></video>
-
-    <script>
-    let localStream;
-
-    // カメラ映像取得
-    navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        })
-        .then(stream => {
-            // 成功時にvideo要素にカメラ映像をセットし、再生
-            const videoElm = document.getElementById('my-video');
-            videoElm.srcObject = stream;
-            videoElm.play();
-            // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-            localStream = stream;
-        }).catch(error => {
-            // 失敗時にはエラーログを出力
-            console.error('mediaDevice.getUserMedia() error:', error);
-            return;
-        });
-    
-    //Peer作成
-    const peer = new Peer({
-    key: '3509b80b-8a54-44d8-bcd6-5c5bcc36a50c',
-    debug: 3
-    });
-
-    //PeerID取得
-    peer.on('open', () => {
-        document.getElementById('my-id').textContent = peer.id;
-    });
-
-    // 発信処理
-    document.getElementById('make-call').onclick = () => {
-        const theirID = document.getElementById('their-id').value;
-        const mediaConnection = peer.call(theirID, localStream);
-        setEventListener(mediaConnection);
-    };
-
-    // イベントリスナを設置する関数
-    const setEventListener = mediaConnection => {
-        mediaConnection.on('stream', stream => {
-            // video要素にカメラ映像をセットして再生
-            const videoElm = document.getElementById('their-video')
-            videoElm.srcObject = stream;
-            videoElm.play();
-        });
-    }
-
-    //着信処理
-    peer.on('call', mediaConnection => {
-        mediaConnection.answer(localStream);
-        setEventListener(mediaConnection);
-    });
-
-    //errorイベント
-    peer.on('error', err => {
-        alert(err.message);
-    });
-
-    //closeイベント
-    peer.on('close', () => {
-        alert('通信が切断しました。');
-    });
-    </script>
+    <div class="h-screen">
+        <div class="h-3/5 bg-[#F4F8FA]">
+            <div class="w-screen pt-32 pb-20 px-20">
+                <div class="flex justify-around">
+                    <img src="{{asset('img/usericon/user1.png')}}" alt="自分のアイコン画像" class="rounded-[50%] w-64 h-64">
+                    <img src="{{asset('img/usericon/user2.png')}}" alt="通話相手のアイコン画像" class="rounded-[50%] w-64 h-64">
+                </div>
+                <p class="text-center mx-auto py-4 text-xl text-[#3A3A3A]">呼び出しています...</p>
+            </div>
+        </div>
+        <div class="h-2/5 bg-white">
+                <div class="w-64 text-center mx-auto py-32">
+                    <div class="w-32 h-32 bg-red-600 rounded-[50%] mx-auto">
+                        <input type="button" value="" class=""><div class='flex items-center justify-center'><i class="fa-solid fa-phone fa-4x text-white" style=" transform:rotate(135deg);"></i></div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
+</html>
