0104ad6195ac051c209c1028cfcd19a05a68e53a
junjan-061 <junjan0601@icloud.com>
2022-05-10 15:18:01 +0900

    チケット消費、購入画面作成

--- laravel/resources/views/search/index.blade.php
+++ laravel/resources/views/search/index.blade.php
@@ -3,22 +3,6 @@
 @section('title', 'メンター検索画面')
 
 @section('content')
-    {{-- <div class="w-8/12 h-2/4 flex justify-center items-center">
-        <form action="" enctype="multipart/form-data" method="POST">
-            @csrf
-            <h2>メンターを探す</h2>
-            <p>社名</p>
-            <input type="text" name="name" placeholder="社名を入力してください" value="">
-            <p>部署名</p>
-            <input type="text" name="department" placeholder="部署名を入力してください" value="">
-            <input type="submit" value="検索する">
-        </form>
-    </div> --}}
-    <p> ・枠線を灰色にする
-        ・inputタグの中身を白くする
-        ・検索ボタンとinputタグを同じ縦横幅にする
-        ・「メンターを探す」を左寄せにする
-        ・下の線を消す・文字を灰色にする</p>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -35,14 +19,15 @@
                         <form method="POST" action="">
                             @csrf
 
-                            <div class="form-group row">
+                            <div class="form-group row bg-gray-200">
                                 <label for="email"
                                     class="col-md-4 col-form-label text-gray-300 text-md-right font-bold flex items-center mr-5">{{ __('社名') }}</label>
 
                                 <div class="col-md-6 flex justify-center items-center">
                                     <input id="" type="text"
-                                        class="bg-white outline outline-gray-400 mb-6 mt-2 w-full h-10 p-2 form-control" name="email"
-                                        value="" required autocomplete="email" autofocus placeholder="社名を入力してください">
+                                        class="bg-white outline outline-gray-400 mb-6 mt-2 w-full h-10 p-2 form-control"
+                                        name="email" value="" required autocomplete="email" autofocus
+                                        placeholder="社名を入力してください">
                                 </div>
                             </div>
 
@@ -52,24 +37,15 @@
 
                                 <div class="col-md-6 flex justify-center items-center">
                                     <input id="" type="text"
-                                        class="bg-white outline outline-gray-400 mb-6 mt-2 w-full h-10 p-2 form-control" name="email"
-                                        value="" required autocomplete="email" autofocus placeholder="部署名を入力してください">
+                                        class="bg-white outline outline-gray-400 mb-6 mt-2 w-full h-10 p-2 form-control"
+                                        name="email" value="" required autocomplete="email" autofocus
+                                        placeholder="部署名を入力してください">
                                 </div>
                             </div>
-
-                            {{-- <div class="form-group row">
-                                <label for="password"
-                                    class="flex items-centercol-md-4 h-10 text-gray-300 mr-4 col-form-label text-md-right font-semibold">{{ __('部署名') }}</label>
-
-                                <div class="col-md-6 flex justify-center items-center">
-                                    <input id="" type="text"
-                                        class="bg-white mb-6 mt-2 outline-gray-400 w-full h-10 p-2 form-control" name="password"
-                                        required autocomplete="current-password" placeholder="部署名を入力してください">
-                                </div>
-                            </div> --}}
                             <div class="form-group row">
                                 <div class="col-md-6 flex justify-center items-center">
-                                    <button class=" bg-gradient-to-r from-sky-500 to-indigo-500 outline-gray-500 text-white text-2xl font-bold w-full h-10 p-2 mt-2  rounded form-control">
+                                    <button
+                                        class=" bg-gradient-to-r from-sky-500 to-indigo-500 outline-gray-500 text-white text-2xl font-bold w-full h-10 mt-2  rounded form-control">
                                         <i class="fas fa-search text-white "></i> 検索する
                                     </button>
                                 </div>
