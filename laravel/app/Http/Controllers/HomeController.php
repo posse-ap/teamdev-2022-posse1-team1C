9d4e6d1a8f088544a761247e666c0e57da9183d9
taiki455 <74663828+taiki455@users.noreply.github.com>
2022-05-15 21:14:45 +0900

    Update HomeController.php

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -26,9 +26,4 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function mentee_register()
-    {
-        return view('auth.mentee_register');
-    }
-
 }
