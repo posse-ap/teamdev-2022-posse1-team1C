76e36edd231c9ca97c9b0015d7a204a0332b6b86
taiki <dannotaiki1113@gmail.com>
2022-05-28 15:33:18 +0900

    アンケートの修正

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -25,10 +25,10 @@ Auth::routes();
 Route::get('/mentee/register', 'MenteeController@register')->name('mentee.register');
 Route::get('/mentee/register-confirm', 'MenteeController@register_confirm')->name('mentee.register_confirm');
 Route::get('/mentee/profile/edit', 'MenteeController@edit_profile')->name('mentee.profile_edit');
-Route::get('/mentor/profile/edit', 'MentorController@edit_profile')->name('mentor.profile_edit');
-Route::get('/mentor/register-confirm', 'MentorController@register_confirm')->name('mentor.register');
 Route::get('/mentee/inquiry', 'MenteeController@inquiry')->name('mentee.inquiry');
 Route::get('/mentee/request-list', 'MenteeController@request_list')->name('mentee.request_list');
+Route::get('/mentor/profile/edit', 'MentorController@edit_profile')->name('mentor.profile_edit');
+Route::get('/mentor/register-confirm', 'MentorController@register_confirm')->name('mentor.register');
 
 Route::get('/chat', 'ChatController@index')->name('chat')->middleware('auth');
 
