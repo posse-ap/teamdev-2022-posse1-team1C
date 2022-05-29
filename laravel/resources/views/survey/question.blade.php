8027a6e6d17b8f1f9df227976947d1f5b1baceca
taiki <dannotaiki1113@gmail.com>
2022-05-29 23:34:44 +0900

    アンケートのroute修正

--- laravel/resources/views/survey/question.blade.php
+++ laravel/resources/views/survey/question.blade.php
@@ -10,8 +10,8 @@
                     <div class="bg-white inline-block px-20 py-20 ml-20">
                         <i class="flex justify-center items-center fa-solid fa-calendar-check fa-3x mb-6"></i>
                         <p class="flex justify-center items-center font-bold mb-16">相談はできましたか？</p>
-                            <input type="button" value="できた" class="bg-teal-400 text-white rounded px-12 py-10">
-                            <input type="button" value="できなかった" class="bg-red-400 text-white rounded px-7 py-10 ml-10">
+                            <input type="button" value="できた" class="bg-teal-400 text-white rounded px-12 py-10" onclick="location.href='{{route('mentee.survey.reason')}}'">
+                            <input type="button" value="できなかった" class="bg-red-400 text-white rounded px-7 py-10 ml-10" onclick="location.href='{{route('mentee.survey.cancel')}}'">
                     </div> 
                </div>
            </div>
