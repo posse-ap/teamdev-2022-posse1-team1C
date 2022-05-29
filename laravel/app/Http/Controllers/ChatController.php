10aee0671aa5b44aa6a53ce1e1fb09dc823eee63
ryudai <bigdragon.06102001@gmail.com>
2022-05-29 12:24:50 +0900

    thread_idによるチャットの出し分けができる。

--- laravel/app/Http/Controllers/ChatController.php
+++ laravel/app/Http/Controllers/ChatController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Thread;
 use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
-    public function index()
+    public function mentee_chat($thread_id)
     {
-        $user = Auth::user();
-        $thread_id = 1;
-        $is_mentor = $user->is_mentor;
-        return view('chat.index', compact('thread_id', 'is_mentor'));
+        $user_id = Auth::id();
+        $thread = Thread::find($thread_id);
+        $is_mentor = 0;
+        if (isset($thread) && $thread->mentee_user_id === $user_id) {
+            return view('chat.index', compact('thread_id', 'is_mentor'));
+        } else {
+            // TODO:back()に変更
+            return redirect('top');
+        }
+    }
+
+    public function mentor_chat($thread_id)
+    {
+        $user_id = Auth::id();
+        $thread = Thread::find($thread_id);
+        $is_mentor = 1;
+        if (isset($thread) && $thread->mentor_user_id === $user_id) {
+            return view('chat.index', compact('thread_id', 'is_mentor'));
+        } else {
+            // TODO:back()に変更
+            return redirect('top');
+        }
     }
 }
